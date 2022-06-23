d8597c0b15836279d813fd8c0b7daab6a69ee3c1
stefanom13 <stefanomancuso13@gmail.com>
2022-06-23 15:09:47 +0200

    classificata stampata

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibi = { "Hamburger", "Pizza", "Pasta", "Insalata di Mare", "Parmigiana", "Riso con Tonno" };
+// listaCibi e stampata lunghezza //
 
+string[] cibiPref = { "Hamburger", "Pizza", "Pasta", "Insalata di Mare", "Parmigiana", "Riso con Tonno" };
 
-Console.WriteLine("\nLunghezza della classifica: " + cibi.Length);
+Console.WriteLine("\nLunghezza della classifica: " + cibiPref.Length);
+
+//stampata classifica //
+
+Console.WriteLine("\nCLASSIFICA: ");
+for (int i = 0; i < cibiPref.Length; i++)
+{
+    Console.WriteLine(i + 1 + " - " + cibiPref[i]);
+}
 
 
