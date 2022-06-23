8760298bb244437563a87d7aa8c5c484c3c6c65b
stefanom13 <stefanomancuso13@gmail.com>
2022-06-23 15:17:38 +0200

    stampato cibo preferito, peggiore e nel mezzo

--- Program.cs
+++ Program.cs
@@ -14,4 +14,20 @@ for (int i = 0; i < cibiPref.Length; i++)
     Console.WriteLine(i + 1 + " - " + cibiPref[i]);
 }
 
+//stampata cibi  migliori peggiori e al centro //
 
+if (cibiPref != null && cibiPref.Length > 0)
+{
+    //stampo cibo preferito //
+    Console.WriteLine("\nCibo preferito: " + cibiPref[0]);
+
+    //stampo cibo meno preferito //
+    Console.WriteLine("\nCibo meno preferito: " + cibiPref[cibiPref.Length - 1]);
+
+    //stampo cibo nel mezzo //
+    if (cibiPref.Length % 2 == 0)
+    {
+        Console.WriteLine("\nCibo mediani: " + cibiPref[cibiPref.Length / 2 - 1] + " e " + cibiPref[cibiPref.Length / 2]);
+    }
+  
+}
